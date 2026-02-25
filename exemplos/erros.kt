edf345c31169602c24f27626884a8ed0ab98f66c
Matheus Fernandes <154653138+thiprer497@users.noreply.github.com>
2026-02-25 04:08:59 -0800

    Update erros.kt

--- exemplos/erros.kt
+++ exemplos/erros.kt
@@ -4,22 +4,22 @@
  */
 
 fun testeErros() {
-    val a = 10
+    val a = 10;
     
     // ERRO 1: Caractere inválido (^)
-    val potencia = a ^ 2 
+    val potencia = a ^ 2;
 
     // ERRO 2: Caractere inválido (~) e (@ solto)
-    val teste = ~a
-    val email = usuario @ dominio
+    val teste = ~a;
+    val email = usuario @ dominio;
 
     // ERRO 3: String não fechada na mesma linha (se for string simples)
     val texto = "String quebrada
-    na linha de baixo"
+    na linha de baixo";
 
     // ERRO 4: Char literal inválido (vazio ou com mais de 1 char)
-    val c1 = ''
-    val c2 = 'ab'
+    val c1 = '';
+    val c2 = 'ab';
 
-    println("Se você viu os erros acima e chegou aqui, o Modo Pânico funcionou!")
+    println("Se você viu os erros acima e chegou aqui, o Modo Pânico funcionou!");
 }
